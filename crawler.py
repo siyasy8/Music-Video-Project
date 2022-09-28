7ac6ca35ad331faf8edcc5e510de9b03d972110b
Siya Yeolekar <siyasy8@ad.unc.edu>
2022-09-27 23:10:28 -0400

    trial

--- crawler.py
+++ crawler.py
@@ -1,5 +1,5 @@
-from pytube import YouTube
+import pytube
 
-yt = YouTube('https://www.youtube.com/watch?v=RzhAS_GnJIc')
+yt = YouTube('https://www.youtube.com/watch?v=wIft-t-MQuE&list=PLwGoz2ybZuMT0WgPXPKvrxX2cx-u8WAOX&ab_channel=TaylorSwiftVEVO')
 stream = yt.streams.get_by_itag(135)
 stream.download()
